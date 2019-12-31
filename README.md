d197b591a029a515121378adc6a8c0b910bc9db8
lijiaocn <lijiaocn@foxmail.com>
2019-12-31 14:58:22 +0800

    rename: Version

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Go 语言深度入门 - 代码
 ======================
 
-[代码要如何组织？](https://www.lijiaocn.com/go-detail/article/pkg.html)
+关联章节：[代码要如何组织？](https://www.lijiaocn.com/go-detail/article/pkg.html)
--- version.go
+++ version.go
@@ -1,4 +1,4 @@
-// Create: 2019/06/04 19:21:00 Change: 2019/12/31 14:50:52
+// Create: 2019/06/04 19:21:00 Change: 2019/12/31 14:58:10
 // FileName: version.go
 // Copyright (C) 2019 lijiaocn <lijiaocn@foxmail.com>
 //
@@ -6,6 +6,6 @@
 
 package mod
 
-func Vesrion() {
+func Version() {
 	println("v0.0.0")
 }
