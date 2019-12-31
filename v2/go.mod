24447af7d012ffbaf8f2642744f613600cffea2e
lijiaocn <lijiaocn@foxmail.com>
2019-12-31 15:04:41 +0800

    release v2.0.0

---
+++ v2/go.mod
@@ -0,0 +1,3 @@
+module lijiaocn.com/go/example/mod/v2
+
+go 1.13
---
+++ v2/version.go
@@ -0,0 +1,11 @@
+// Create: 2019/06/04 19:21:00 Change: 2019/12/31 15:03:19
+// FileName: version.go
+// Copyright (C) 2019 lijiaocn <lijiaocn@foxmail.com>
+//
+// Distributed under terms of the GPL license.
+
+package v2
+
+func Version() {
+	println("v2.0.0")
+}
