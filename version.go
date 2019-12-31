c0315ecb4f280206fb9c7f36cceae7c39fb4f964
lijiaocn <lijiaocn@foxmail.com>
2019-12-31 14:58:57 +0800

    release v1.0.0

--- version.go
+++ version.go
@@ -1,4 +1,4 @@
-// Create: 2019/06/04 19:21:00 Change: 2019/12/31 14:58:10
+// Create: 2019/06/04 19:21:00 Change: 2019/12/31 14:58:36
 // FileName: version.go
 // Copyright (C) 2019 lijiaocn <lijiaocn@foxmail.com>
 //
@@ -7,5 +7,5 @@
 package mod
 
 func Version() {
-	println("v0.0.0")
+	println("v1.0.0")
 }
